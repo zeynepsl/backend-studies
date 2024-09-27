962bc16b553d8f14cece696563e320d03c0a6832
zsalman <zsalman@protel.com.tr>
2024-09-27 11:36:41 +0300

    upd

--- querydsl-demo/study-cases/src/main/java/com/zeynep/study_cases/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zeynep.study_cases.repository;
-
-import com.zeynep.study_cases.model.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
